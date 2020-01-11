700944a3b37d21e015250089df7b52b90bc52882
Saifur Rahman <saifur703@gmail.com>
2020-01-11 21:52:20 +0600

    Create variable.php

---
+++ variable.php
@@ -0,0 +1,14 @@
+<?php 
+
+// variable
+$var1 = "Hello ";
+$var2 = "World";
+echo $var1.$var2;
+
+$var3 = 123;
+
+// Constant
+define("PI", 3.14159);
+echo("PI Value is: ").PI;
+// Another way
+echo constant('PI');
