70e5170acd946d7808e310f5b9b65a2974a9dc8d
Saifur Rahman <saifur703@gmail.com>
2020-01-13 12:32:28 +0600

    Create assignment-operator.php

---
+++ assignment-operator.php
@@ -0,0 +1,42 @@
+// Assignment Operator
+
+/*
+
++=
+-=
+*=
+/=
+%=
+
+*/
+
+// ADD
+$num1 = 22;
+$num2 = 11;
+$num1 += $num2;
+echo $num1;
+
+// MINUS
+$num3 = 55;
+$num4 = 39;
+$num3 -= $num4;
+echo $num3;
+
+// INTO
+$x1 = 72;
+$x2 = 5;
+$x1 *= $x2;
+echo $x1;
+
+// DIVIDE
+$a = 25;
+$b = 5;
+$a /= $b;
+echo $a;
+
+// MODULAS
+$xy = 36;
+$yz = 8;
+$xy %= $yz;
+echo $xy;
+
