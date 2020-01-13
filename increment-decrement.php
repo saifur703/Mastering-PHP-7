54cd140cc66d04215f66edc0ca833c34d0ffe79a
Saifur Rahman <saifur703@gmail.com>
2020-01-13 13:01:12 +0600

    Create increment-decrement.php

---
+++ increment-decrement.php
@@ -0,0 +1,39 @@
+<?php 
+
+// Increment
+$n = 11;
+$n++
+echo $n;
+
+// Decrement
+$m = 42;
+$m--;
+echo $m;
+
+
+// Another Way
+
+$n1 = 35;
+$m1 = $n1++;
+echo $m1, $n1;
+
+$n2 = 76;
+$m2 = $n2--;
+echo $m2, $n2;
+
+/*
+
+// Theory
+$m = $n++
+      meaning:-
+$m = $n;
+$n+=1;
+
+$m = ++$n;
+      meaning:-
+$n+=1;
+$m = n;
+
+*/
+
+// As same as decrement Theory.
