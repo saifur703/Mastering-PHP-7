b6c5fe1b5a0eface60ff021b69b314ca6f54adb1
Saifur Rahman <saifur703@gmail.com>
2020-01-13 12:27:26 +0600

    Create arithmetic-operator.php

---
+++ arithmetic-operator.php
@@ -0,0 +1,44 @@
+
+// Arithmetic Operator
+/*
+
++       Plus
+-       Minus
+*       Multiplication
+/       Division
+%       Modulas
+
+*/
+
+// Example:
+
+// ADD
+$num1 = 22;
+$num2 = 11;
+$add = $num1 + $num2;
+echo $add;
+
+// MINUS
+$num3 = 55;
+$num4 = 39;
+$minus = $num3 - $num4;
+echo $minus;
+
+// INTO
+$x1 = 72;
+$x2 = 5;
+$into = $x1 * $x2;
+echo $into;
+
+// DIVIDE
+$a = 25;
+$b = 5;
+$divide = $a / $b;
+echo $divide;
+
+// MODULAS
+$xy = 36;
+$yz = 8;
+$modulas = $xy % $yz;
+echo $modulas;
+
